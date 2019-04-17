41e339642d029a18de3159e0da4524fbfb0fdabb
Pantala Labs <gibrancurtiss@gmail.com>
2019-04-17 19:40:01 -0300

    Add files via upload

--- eurorack_quantizer.ino
+++ eurorack_quantizer.ino
@@ -3,41 +3,44 @@
   Quantizer by Pantala Labs is licensed under a
   Creative Commons Attribution 4.0 International License.
   Gibran Curtiss SalomÃ£o. MAY/2017 - CC-BY-SA
-
-  TO-DO: 
-  a)calibrate the final voltage on CV out using a table for each midi note
 */
 #include <Wire.h>
 #include <Adafruit_MCP4725.h>
-Adafruit_MCP4725 dac;
 #include <MIDI.h>
 MIDI_CREATE_DEFAULT_INSTANCE();
-
-#define TRIGGERPIN 2
-#define QTZOPTION 3
-#define CVINPIN A6
-boolean pleaseQuantizeMe;
-int lastNote = 0;
-int cvRead;
-int noteQuantized;
-int voltageQuantized;
+Adafruit_MCP4725 dac;
+//#define DAC_RESOLUTION (12)
 
 boolean debugMe = false;
 
+#define TRIGGERPIN 2
+#define QTZPIN 3
+#define CVINPIN A6
+#define ZEROEQUIVALENT 4
+#define FOUROCTAVES_MIDI59_4916V 4084
+
+volatile boolean pleaseQuantizeMe;
+int lastNote = 0;
+
 void setup()
 {
+
+  pinMode(QTZPIN, INPUT);
   pinMode(TRIGGERPIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(TRIGGERPIN), tickInterrupt, FALLING);
-  pinMode(QTZOPTION, INPUT);
+  attachInterrupt(digitalPinToInterrupt(TRIGGERPIN), tickInterrupt, RISING);
+  dac.begin(0x62);
+  dac.setVoltage(0, false);
   if (debugMe)
-  {
     Serial.begin(9600);
-  }
   else
-  {
-    dac.begin(0x62);
     MIDI.begin();
-  }
+
+  // for (uint16_t v = 3900; v < 4095; v++)
+  // {
+  //   Serial.println(v);
+  //   dac.setVoltage(v, false);
+  //   delay(1000);
+  // }
 }
 
 void tickInterrupt()
@@ -47,19 +50,17 @@ void tickInterrupt()
 
 void loop()
 {
-  cvRead = analogRead(CVINPIN);
+  uint16_t cvRead = analogRead(CVINPIN);
   if (pleaseQuantizeMe)
   {
     pleaseQuantizeMe = false;
-    noteQuantized = map(cvRead, 0, 1023, 0, 60);
-    if (digitalRead(QTZOPTION))
-    {
-      voltageQuantized = map(noteQuantized, 0, 60, 0, 4095);
-    }
+    uint16_t noteQuantized;
+    uint16_t voltageQuantized;
+    noteQuantized = constrain(map(cvRead, 0, 1023, 0, 60), 0, 59); //read 5 volts but constrain to 4.911V = 4 octaves
+    if (digitalRead(QTZPIN))
+      voltageQuantized = map(noteQuantized, 0, 59, ZEROEQUIVALENT, FOUROCTAVES_MIDI59_4916V);
     else
-    {
       voltageQuantized = cvRead << 2;
-    }
     MIDI.sendNoteOff(lastNote, 0, 1);
     MIDI.sendNoteOn(noteQuantized, 127, 1);
     dac.setVoltage(voltageQuantized, false);
